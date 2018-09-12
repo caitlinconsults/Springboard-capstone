df0f3c2178abcb9c891782cd71c676c8288406b8
Caitlin Davies <42181861+caitlinrdavies@users.noreply.github.com>
2018-09-11 19:41:10 -0600

    Improve clustering code

--- 7.4 Apply Machine Learning to Capstone.R
+++ 7.4 Apply Machine Learning to Capstone.R
@@ -1,20 +1,24 @@
 ### Clustering Climate Stations by Latitude, Longitude, and Elevation
 
-# install packages
+### Is there a way to preference one or two data features over others when clustering? 
+
+# Install packages
 
 install.packages(c("cluster", "NbClust"))
 library("cluster")
 library("NbClust")
+library("dplyr")
+library("ggmap")
 
-# Create Station Cluster Data
+# Scale data for clustering
 
-Stations.cluster <- Stations[, c(4:6)]
-Stations.cluster <- scale(Stations.cluster)
+Stations.cluster <- Stations[, c(1, 4:6)]
+Stations.cluster[c(2:4)] <- lapply(Stations.cluster[2:4], function(x) c(scale(x)))
 
-# How many clusters does this method suggest? It could be 3 or 4? 
+# Determine number of clusters: Method 1
 
 wssplot <- function(data, nc = 8, seed = 1234){
-  wss <- (nrow(data) - 1)*sum(apply(data, 2, var))
+  wss <- (nrow(data) - 1) * sum(apply(data, 2, var))
   for (i in 2:nc){
     set.seed(seed)
     wss[i] <- sum(kmeans(data, centers = i)$withinss)}
@@ -23,28 +27,33 @@ wssplot <- function(data, nc = 8, seed = 1234){
        ylab = "Within groups sum of squares")
 }
 
-wssplot(Stations.cluster)
+wssplot(Stations.cluster[c(2:4)])
 
-k = 4
-set.seed(1)
-fit.km <- kmeans(Stations.cluster, centers = k, iter.max = 1000)
-
-# What do these results mean? 
-
-Stations.Results <- table(fit.km$cluster, Stations$Climate.Division)
-View(Stations.Results)
-
-# Why am I getting an error here? 
-
-clusplot(Stations.Results, clus = Stations.Results[2, ])
-
-# Using another method to verify number of clusters
+# Determine number of clusters: Method 2
 
 set.seed(1234)
-nc <- NbClust(Stations.cluster, min.nc = 2, max.nc = 8, method = "kmeans")
+nc <- NbClust(Stations.cluster[2:4], min.nc = 2, max.nc = 8, method = "kmeans")
 barplot(table(nc$Best.n[1, ]),
         xlab = "Number of Clusters", ylab = "Number of Criteria",
         main = "Number of Clusters Chosen")
+
+# Apply k-means clustering
+
+k = 4
+set.seed(1234)
+fit.km <- kmeans(Stations.cluster[c(2:4)], centers = k, iter.max = 1000)
+
+# Add clusters to Stations dataset
+
+Stations$Cluster <- as.factor(fit.km$cluster)
+View(Stations)
+
+# View clusters on a map
+
+MN_Map <- get_map("Minnesota", zoom = 6)
+
+ggmap(MN_Map) + 
+  geom_point(aes(x = Longitude, y = Latitude, color = Cluster), data = Stations)
 
 ### Predicting future winter tempuratures using Linear Regression
 
